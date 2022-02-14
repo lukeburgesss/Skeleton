b8b95e2c9fea60e63f99dba981be5ba2d5a2fa4d
Muhammad-Amin Allana <P2592692@my365.dmu.ac.uk>
2022-02-14 12:30:36 +0000

    Made a test for each of my table components in tblSupplier and the class used was clsSupplier

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,26 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        // SupplierID
+        public DateTime DateAdded { get; set; }
+
+        // OrderDate
+        public int SupplierID { get; set; }
+
+        // ProductName
+        public string Street { get; set; }
+        
+        // ProductQuantity 
+        public int ProductQuantity { get; set; }
+
+        // Purchase
+        public bool Purchase { get; set; }
+
+        // Price
+        public decimal Price { get; set; }
+
     }
 }
