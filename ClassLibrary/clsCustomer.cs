7879c0c3bc9eb755d999877928d51fde67be175c
Eliseo-Eugenio Leonenco <P2606812@my365.dmu.ac.uk>
2022-02-19 18:34:20 +0000

    Added tests for all Customer class attributes and created properties to pass the tests

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool ActiveAccount { get; set; }
+        public DateTime CustomerDob { get; set; }
+        public int CustomerNo { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerContactNo { get; set; }
+        public string DeliveryAddr { get; set; }
     }
 }
