04c79e1c57d96e8fdb17edb444e6ebb2f62e2c19
Muhammad-Amin Allana <P2592692@my365.dmu.ac.uk>
2022-02-21 12:42:56 +0000

    Week21 Lab
    - Added the Data Entry code got it to work and read the text input from the text box.
    
    - Got the SupplierManagementViewer working and display on the web page working

--- AdminSystem/SupplierManagementViewer.aspx.cs
+++ AdminSystem/SupplierManagementViewer.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // create a new instance of the class 
+        clsSupplier aSupplier = new clsSupplier();
+
+        // get the data from the session object 
+        aSupplier = (clsSupplier)Session["aSupplier"];
+
+        // display the supplier number on the page
+        Response.Write(aSupplier.SupplierName);
 
     }
 }
