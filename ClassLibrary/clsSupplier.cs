fe581de794c9859251ae08be6675591c6a24d84d
Muhammad-Amin Allana <P2592692@my365.dmu.ac.uk>
2022-03-07 21:01:38 +0000

    My edited and and complete presentation layer work Week 21

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -5,17 +5,17 @@ namespace ClassLibrary
     public class clsSupplier
     {
         // SupplierID
-        public DateTime DateAdded { get; set; }
+        public int SupplierID { get; set; }
 
         // SupplierDateAdded
-        public int SupplierID { get; set; }
+        public DateTime SupplierDateAdded { get; set; }
 
         // SupplierName
         public string SupplierName { get; set; }
        
 
         // Archive
-        public bool Purchase { get; set; }
+        public bool SupplierArchive { get; set; }
 
     }
 }
