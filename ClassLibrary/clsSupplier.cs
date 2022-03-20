6104b6a15473c69a98af4d3325d9438779ba3e65
Muhammad-Amin Allana <P2592692@my365.dmu.ac.uk>
2022-03-20 13:35:38 +0000

    clsDataConnection for supplier test and class

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -113,15 +113,46 @@ namespace ClassLibrary
         }
 
         //------------------- SUPPLIER TESTING --------------------
-        public bool Find(int supplierID)
+
+        public bool Find(int SupplierID)
         {
-            //set the private data members to the test data value
-            aSupplierID = 21;
-            aSupplierName = "Test Name";
-            aSupplierDateAdded = Convert.ToDateTime("28/05/2002");
-            aSupplierArchive = true;
-            //always return true
-            return true;
+
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the employee no to search for
+            DB.AddParameter("@SupplierID", SupplierID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblSupplier_FilterBySupplierID");
+            //if one record is found there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                // copy the data from the database to the private data members
+                aSupplierID = Convert.ToInt32(DB.DataTable.Rows[0]["SupplierID"]);
+                aSupplierName = Convert.ToString(DB.DataTable.Rows[0]["SupplierName"]);
+                aSupplierDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["SupplierDateAdded"]);
+                aSupplierArchive = Convert.ToBoolean(DB.DataTable.Rows[0]["SupplierArhive"]);
+                //return that everything worked OK
+                return true;
+                //if no record was found
+            }
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+
+            /*
+            public bool Find(int supplierID)
+            {
+                //set the private data members to the test data value
+                aSupplierID = 21;
+                aSupplierName = "Test Name";
+                aSupplierDateAdded = Convert.ToDateTime("28/05/2002");
+                aSupplierArchive = true;
+                //always return true
+                return true;
+            */
+
         }
     }
 }
