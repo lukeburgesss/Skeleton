ab77145f9fdbc36e5cff8cd5126eb280ec5ce4ce
Muhammad-Amin Allana <P2592692@my365.dmu.ac.uk>
2022-03-29 17:46:31 +0100

    Wk26 Complete

--- AdminSystem/SupplierManagementList.aspx
+++ AdminSystem/SupplierManagementList.aspx
@@ -9,6 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:ListBox ID="lstSupplierList" runat="server" OnSelectedIndexChanged="lstSupplierList_SelectedIndexChanged"></asp:ListBox>
         </div>
     </form>
 </body>
--- ClassLibrary/clsSupplierCollection.cs
+++ ClassLibrary/clsSupplierCollection.cs
@@ -1,6 +1,52 @@
-﻿namespace ClassLibrary
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
 {
     public class clsSupplierCollection
     {
+
+        //private data member 
+        List<clsSupplier> mSupplierList = new List<clsSupplier>();
+
+        clsSupplier mThisSupplier = new clsSupplier();
+
+        public List<clsSupplier> SupplierList
+        {
+            get
+            {
+                return mSupplierList;
+            }
+            set
+            {
+                mSupplierList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mSupplierList.Count;
+            }
+            set
+            {
+                //Sort out later
+            }
+        }
+
+        public clsSupplierCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.Execute("sproc_tblSupplier_SelectAll");
+
+           // PopulateArray(DB);
+
+        }
+
+
+
+
+        public clsSupplier ThisSupplier { get; set; }
     }
 }
